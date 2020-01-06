75c66c76895b923030350b2c82abdcb2297e197c
Simon Katz <sk@nomistech.com>
2020-01-06 19:04:41 +0000

    Add stuff to reduce problems caused by long lines in REPL buffer

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . (;; Stuff to reduce problems caused by long lines in REPL buffer.
+         (truncate-lines . t)
+         (cider-show-eval-spinner . nil)
+         (cider-font-lock-max-length . 1000))))
